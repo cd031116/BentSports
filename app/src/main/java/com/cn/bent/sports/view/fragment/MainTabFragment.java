36b4bb68753f9f16297e91c7bcedfc282f49e0a4
lyj <2747798729@qq.com>
2018-01-29 15:13:30 +0800

    push

--- app/src/main/java/com/cn/bent/sports/view/fragment/MainTabFragment.java
+++ app/src/main/java/com/cn/bent/sports/view/fragment/MainTabFragment.java
@@ -2,13 +2,17 @@ package com.cn.bent.sports.view.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.View;
+
+import com.cn.bent.sports.R;
+import com.cn.bent.sports.base.BaseFragment;
 
 /**
  * Created by lyj on 2018/1/29 0029.
  * description
  */
 
-public class MainTabFragment extends Fragment {
+public class MainTabFragment extends BaseFragment {
 
     public static MainTabFragment newInstance(int type) {
         MainTabFragment fragment = new MainTabFragment();
@@ -18,4 +22,18 @@ public class MainTabFragment extends Fragment {
         return fragment;
     }
 
+    @Override
+    protected int getLayoutId() {
+        return R.layout.main_fragment;
+    }
+
+    @Override
+    protected void initView(View view, Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
 }
