9941e0638a99e3c434bd45588a44ac7e63ec6d55
lyj <2747798729@qq.com>
2018-01-29 16:07:39 +0800

    push

---
+++ app/src/main/java/com/cn/bent/sports/recyclebase/ItemViewDelegate.java
@@ -0,0 +1,16 @@
+package com.cn.bent.sports.recyclebase;
+
+
+/**
+ * Created by lyj on 16/6/22.
+ */
+public interface ItemViewDelegate<T>
+{
+
+    int getItemViewLayoutId();
+
+    boolean isForViewType(T item, int position);
+
+    void convert(ViewHolder holder, T t, int position);
+
+}
