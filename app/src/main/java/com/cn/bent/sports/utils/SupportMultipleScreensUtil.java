5c15f0b39824cbf23fb8f8bd4586cffda76e47ed
lyj <2747798729@qq.com>
2018-02-02 10:48:57 +0800

    push

--- app/src/main/java/com/cn/bent/sports/utils/SupportMultipleScreensUtil.java
+++ app/src/main/java/com/cn/bent/sports/utils/SupportMultipleScreensUtil.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
