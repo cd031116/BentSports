a383e1b280f9742f43f1bf86cb872722b159fe02
dawn4it <2229713767@qq.com>
2018-02-06 10:00:59 +0800

    add d

--- app/src/main/java/com/cn/bent/sports/view/fragment/PlayFragment.java
+++ app/src/main/java/com/cn/bent/sports/view/fragment/PlayFragment.java
@@ -453,7 +453,7 @@ public class PlayFragment extends BaseFragment implements AMap.OnMarkerClickList
         } else {
             String distance = String.valueOf(AMapUtils.calculateLineDistance(mStartPoint, mEndPoint));
             juli.setText(AMapUtil.getFriendlyLength((int) (Double.parseDouble(distance))));
-            if ((int) (Double.parseDouble(distance)) < 20) {
+            if ((int) (Double.parseDouble(distance)) < 30*1000) {
                 //打开蓝牙
                 checkBluetooth();
             }
@@ -597,7 +597,8 @@ public class PlayFragment extends BaseFragment implements AMap.OnMarkerClickList
                     line_s.setVisibility(View.GONE);
                     go_task.setVisibility(View.VISIBLE);
                     //弹游戏
-                    mMinewBeaconManager.stopScan();
+                    if (mMinewBeaconManager != null)
+                        mMinewBeaconManager.stopScan();
                 }
             }
 
