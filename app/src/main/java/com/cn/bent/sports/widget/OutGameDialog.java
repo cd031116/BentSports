8a8fc33ffe8ed5e547bf718abe1f02fb878b26f6
dawn <2229713767@qq.com>
2018-03-26 16:37:28 +0800

    add layout

---
+++ app/src/main/java/com/cn/bent/sports/widget/OutGameDialog.java
@@ -0,0 +1,78 @@
+package com.cn.bent.sports.widget;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.cn.bent.sports.R;
+import com.cn.bent.sports.utils.SupportMultipleScreensUtil;
+
+/**
+ * Created by dawn on 2018/3/24.
+ */
+
+public class OutGameDialog extends Dialog implements View.OnClickListener {
+    private TextView out_game;
+    private TextView quxiao;
+    private Context mContext;
+    private OnCloseListener listener;
+
+
+    public OutGameDialog(Context context) {
+        super(context);
+        this.mContext = context;
+    }
+
+
+    public OutGameDialog(Context context, int themeResId) {
+        super(context, themeResId);
+        this.mContext = context;
+    }
+
+    public OutGameDialog(Context context, int themeResId, OnCloseListener listener) {
+        super(context, themeResId);
+        this.mContext = context;
+        this.listener = listener;
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.out_game_dialog_layout);
+        View rootView = findViewById(android.R.id.content);
+        SupportMultipleScreensUtil.init(mContext);
+        SupportMultipleScreensUtil.scale(rootView);
+        setCanceledOnTouchOutside(false);
+        setCancelable(false);
+        initView();
+    }
+
+    private void initView() {
+        out_game = (TextView) findViewById(R.id.out_game);
+        quxiao = (TextView) findViewById(R.id.quxiao);
+        out_game.setOnClickListener(this);
+        quxiao.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.out_game:
+                listener.onClick(this, "");
+                break;
+            case R.id.quxiao:
+                listener.onClick(this, "");
+                break;
+        }
+    }
+
+    public interface OnCloseListener {
+        void onClick(Dialog dialog, String confirm);
+    }
+}
+
+
