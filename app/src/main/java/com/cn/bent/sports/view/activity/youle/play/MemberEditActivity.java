c342535f49f674f539d074e11a459271f69d068a
dawn <2229713767@qq.com>
2018-03-31 10:27:15 +0800

    add map

---
+++ app/src/main/java/com/cn/bent/sports/view/activity/youle/play/MemberEditActivity.java
@@ -0,0 +1,26 @@
+package com.cn.bent.sports.view.activity.youle.play;
+
+import com.cn.bent.sports.R;
+import com.cn.bent.sports.base.BaseActivity;
+
+/**
+ * Created by dawn on 2018/3/31.
+ */
+
+public class MemberEditActivity extends BaseActivity {
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.luxian_member_layout;
+    }
+
+    @Override
+    public void initView() {
+        super.initView();
+    }
+
+    @Override
+    public void initData() {
+        super.initData();
+    }
+}
